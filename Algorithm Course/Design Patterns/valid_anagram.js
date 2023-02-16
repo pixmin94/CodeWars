2ef943ce9e7e6defd69374732896ea7895b21fcc
corina <pixmin@outlook.com>
2023-02-16 22:28:25 +0800

    Added factorial challenge

--- Algorithm Course/Design Patterns/valid_anagram.js
+++ Algorithm Course/Design Patterns/valid_anagram.js
@@ -22,3 +22,29 @@ function validAnagram(string1, string2){
     // exit loop without false return true
     return true
 }  
+
+function validAnagram2(first, second) {
+    if(first.length !== second.length) {
+        return false
+    }
+
+    const lookup = {}
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i]
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i]
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup[letter]) {
+            return false
+        } else {
+            lookup[letter] -= 1
+        }
+    }
+
+    return true
+}
